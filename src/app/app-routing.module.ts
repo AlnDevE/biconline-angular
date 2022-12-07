cb89c52342c074ef731dfb84e5ae47552f7b42c3
Alan <alanfernando2809@gmail.com>
2022-12-06 23:43:56 -0300

    creating navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'inicio'
-  }
+    component: AppComponent,
+    children:[
+      {
+        path: '',
+        redirectTo: 'inicio'
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
