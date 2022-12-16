080fb80983671c8e59d2d88de094ae6c5b78f8d5
Alan <alanfernando2809@gmail.com>
2022-12-16 00:00:48 -0300

    .

--- src/app/modules/solicitations/solicitations.component.ts
+++ src/app/modules/solicitations/solicitations.component.ts
@@ -13,6 +13,7 @@ export class SolicitationsComponent implements OnInit {
 
   user!: UserInfo;
   path!: string;
+  solicitations!: Solicitation[];
 
   constructor(private authUser: AutenticacaoUsuarioService, private sharedService: SharedService) {
     this.authUser.retornaUsuarioLogado().subscribe(
@@ -22,14 +23,6 @@ export class SolicitationsComponent implements OnInit {
       }
     )
   }
-
-  selectedCustomer1!:any;
-  solicitations: Solicitation[] = [
-    {id:1, data:'15-12-2022',descricao:'Limpar encanação',idCliente: 2, idPrestador: 44, status: 'Resolvido'},
-    {id:22, data:'15-12-2022',descricao:'Limpar encanação',idCliente: 3, idPrestador: 34, status: 'Aguardando'},
-    {id:334, data:'15-12-2022',descricao:'Limpar encanação',idCliente: 4, idPrestador: 70, status: 'Cancelado'},
-    {id:12, data:'15-12-2022',descricao:'Limpar encanação',idCliente: 31, idPrestador: 67, status: 'Resolvido'}
-  ]
 
   ngOnInit(): void {
     this.get_all_solicitations(+this.user.id);
