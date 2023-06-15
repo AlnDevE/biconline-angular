ad23a78361badcdd1ba05581136eaa43f416e42e
Alan <alanfernando2809@gmail.com>
2023-06-15 15:13:31 -0300

    readme: add previews

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // apiURL: 'https://biconlineapp.rj.r.appspot.com/bicoon/',
   apiURL: 'http://localhost:8080/bicoon/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  // apiURL: 'https://biconlineapp.rj.r.appspot.com/bicoon/',
   apiURL: 'http://localhost:8080/bicoon/'
 };
 
